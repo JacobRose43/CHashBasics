a5df31813880a5b4ae0768ad883416388a68f511
JacobRose <95928931+JacobRose43@users.noreply.github.com>
2022-02-09 10:27:39 +0100

    update

--- WpfEditorApp/EditorApp/EditorApp/MainWindow.xaml.cs
+++ WpfEditorApp/EditorApp/EditorApp/MainWindow.xaml.cs
@@ -10,6 +10,8 @@ namespace EditorApp
     public partial class MainWindow : Window
     {
         private OpenFileDialog openFileDialog;
+        private SaveFileDialog saveFileDialog;
+        private string fullPathFileName;
         public MainWindow()
         {
             InitializeComponent();
@@ -18,14 +20,46 @@ namespace EditorApp
             openFileDialog.DefaultExt = "txt";
             openFileDialog.Filter = "Pliki tekstowe |*.txt|Wszystkie Pliku|*.*";
             openFileDialog.FilterIndex = 1;
+
+            saveFileDialog = new SaveFileDialog();
+            saveFileDialog.Title = "Zapisywanie Pliku";
+            saveFileDialog.DefaultExt = "txt";
+            saveFileDialog.Filter = openFileDialog.Filter;
+            saveFileDialog.FilterIndex = 1; 
         }
 
         private void MenuItem_Open_Click(object sender, RoutedEventArgs e)
         {
+            if (!string.IsNullOrWhiteSpace(fullPathFileName))
+            {
+                openFileDialog.InitialDirectory = Path.GetDirectoryName(fullPathFileName);
+                openFileDialog.FileName = Path.GetFileName(fullPathFileName);
+            }
             bool? result = openFileDialog.ShowDialog();
+            //false if click "x" in dialog box;
+
+            if (result.HasValue && result.Value)
+            { 
+                fullPathFileName = openFileDialog.FileName;
+                MainTextBox.Text = File.ReadAllText(fullPathFileName);
+                fileNameTextBlock.Text = Path.GetFileName(fullPathFileName);
+            }
+        }
+
+        private void MenuItem_SaveAs_Click(object sender, RoutedEventArgs e)
+        {
+            if (!string.IsNullOrWhiteSpace(fullPathFileName))
+            {
+                saveFileDialog.InitialDirectory = Path.GetDirectoryName(fullPathFileName);
+                saveFileDialog.FileName = Path.GetFileName(fullPathFileName); 
+            }
+
+            bool? result = saveFileDialog.ShowDialog();
             if (result.HasValue && result.Value)
             {
-                MainTextBox.Text = File.ReadAllText(openFileDialog.FileName);
+                fullPathFileName = saveFileDialog.FileName;
+                File.WriteAllText(fullPathFileName, MainTextBox.Text);
+                fileNameTextBlock.Text = Path.GetFileName(fullPathFileName);
             }
         }
     }
