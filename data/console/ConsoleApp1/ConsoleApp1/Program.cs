73596ef48bd460370d54de7e4cca324753f79000
JacobRose <95928931+JacobRose43@users.noreply.github.com>
2022-03-30 22:46:24 +0200

    update

---
+++ data/console/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+
+
+
+
+
+
+
+
+
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
