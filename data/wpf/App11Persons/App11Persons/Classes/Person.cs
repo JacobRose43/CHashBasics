fb86acfd632d039b6eeab2ba770560dd1f849705
JacobRose <95928931+JacobRose43@users.noreply.github.com>
2022-05-10 21:45:16 +0200

    update

---
+++ data/wpf/App11Persons/App11Persons/Classes/Person.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Runtime.Versioning;
+
+namespace App11Persons.Classes
+{
+    public class Person
+    {
+        private string fName,lName;
+        private int age;
+        public string FName
+        {
+            get => fName;
+            set
+            {
+                if (value == fName || string.IsNullOrWhiteSpace(value))
+                    return;
+                fName = value;
+            }
+        }
+        public string LName
+        {
+            get => lName;
+            set
+            {
+                if (value == lName || string.IsNullOrWhiteSpace(value))
+                    return;
+                lName = value;
+            }
+        }
+        public int Age
+        {
+            get => age;
+            set
+            {
+                if (value == age || value < 0)
+                    return;
+                age = value;
+            }
+        }
+        public Person() { FName = "noname"; LName = "noname"; Age = 0; }
+        public Person(string fn, string ln, int a) : this()
+        {
+            FName = fn;
+            LName = ln;
+            Age = a;
+        }
+        public override string ToString()
+        {
+            return $"{LName} {FName}, {Age}";
+        }
+        public static bool operator ==(Person a, Person b)
+        {
+            return a.FName == b.FName &&
+                a.LName == b.LName &&
+                a.Age == b.Age;
+        }
+        public static bool operator !=(Person a, Person b)
+        {
+            return !(a == b);
+        }
+        public override bool Equals(object obj)
+        {
+            if (obj == null || !(obj is Person))
+                return false;
+            return this == obj as Person;
+        }
+        /*
+        public override int GetHashCode()
+        {
+            return new Random().Next();
+        }
+        */
+    }
+}
---
+++ data/wpf/App11Persons/App11Persons/MainWindow.xaml.cs
@@ -0,0 +1,50 @@
+﻿using App11Persons.Classes;
+using System.Windows;
+
+namespace App11Persons
+{
+    /// <summary>
+    /// Logika interakcji dla klasy MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            AgeSlider.Value = 0;
+            AgeTBlock.Text = "0";
+            FNameTBox.Text = string.Empty;
+            LNameTBox.Text = string.Empty;
+        }
+
+        private void Slider_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+            if (AgeSlider == null || AgeTBlock == null)
+                return;
+            AgeTBlock.Text = ((int)AgeSlider.Value).ToString();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            Person newPerson = new Person(FNameTBox.Text, LNameTBox.Text, (int)AgeSlider.Value);
+
+            if (PersonsLBox.Items.IndexOf(newPerson) == -1)
+                PersonsLBox.Items.Add(newPerson);
+            else
+                MessageBox.Show("Już jest taki!");
+            /*
+            PersonsLBox.Items.Add(
+                new Person(FNameTBox.Text,
+                LNameTBox.Text,
+                (int)AgeSlider.Value));
+            */
+        }
+
+        private void PersonsLBox_SelectionChanged(object sender, System.Windows.Controls.SelectionChangedEventArgs e)
+        {
+            FNameTBox.Text = (PersonsLBox.SelectedItem as Person).FName;
+            LNameTBox.Text = (PersonsLBox.SelectedItem as Person).LName;
+            AgeSlider.Value = (PersonsLBox.SelectedItem as Person).Age;
+        }
+    }
+}
