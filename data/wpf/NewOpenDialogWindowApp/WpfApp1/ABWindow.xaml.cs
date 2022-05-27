7b203649012c8974a97b7faea8aaa8a172e121aa
JacobRose <95928931+JacobRose43@users.noreply.github.com>
2022-05-27 22:52:38 +0200

    update

---
+++ data/wpf/NewOpenDialogWindowApp/WpfApp1/ABWindow.xaml.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace WpfApp1
+{
+    /// <summary>
+    /// Logika interakcji dla klasy ABWindow.xaml
+    /// </summary>
+    public partial class ABWindow : Window
+    {
+        public ABWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            if(!double.TryParse(ATBox.Text,out double a))
+            {
+                MessageBox.Show("Błędna liczba w polu a!");
+                ATBox.Focus();
+                return;
+            }
+            if (!double.TryParse(BTBox.Text, out double b))
+            {
+                MessageBox.Show("Błędna liczba w polu b!");
+                BTBox.Focus();
+                return;
+            }
+            DialogResult = true;
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            DialogResult = false;
+        }
+    }
+}
---
+++ data/wpf/NewOpenDialogWindowApp/WpfApp1/MainWindow.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApp1
+{
+    /// <summary>
+    /// Logika interakcji dla klasy MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {ABWindow abDialog = new ABWindow();
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void buton1_Click(object sender, RoutedEventArgs e)
+        {
+            
+            // zmienna abDialog, 
+            bool? result =  abDialog.ShowDialog();
+            if(result.HasValue && result.Value)
+            {
+                double a = double.Parse(abDialog.ATBox.Text), 
+                       b = double.Parse(abDialog.BTBox.Text);
+                ResultTBlock.Text = $"{a}+{b} ={a+b}";
+            }
+
+        }
+    }
+}
